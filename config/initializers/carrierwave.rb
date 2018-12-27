8260337f008c798e9af5e1c80ef96c26e90ceaa7
merkotanh <galina.merkotan@faceit-team.com>
2018-12-27 14:51:18 +0200

    added fog storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,6 @@
  
   CarrierWave.configure do |config|
-
-    # config.storage = :fog
-    # config.fog_use_ssl_for_aws = true
-    # config.fog_directory  = ENV['S3_BUCKET']
-    # config.fog_public     = true
-    # config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
-    # config.asset_host = 'https://s3.amazonaws.com/mygalleryapp'
-
-  	config.fog_provider = 'fog/aws'  
-  
+ 	config.fog_provider = 'fog/aws'  
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
