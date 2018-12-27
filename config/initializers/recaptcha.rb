c34fd12c1a4118723688dcfa177c23a09460bda5
merkotanh <galina.merkotan@faceit-team.com>
2018-12-27 15:56:54 +0200

    added fog storage

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LdoIl8UAAAAAA-GeR-bq7MlhcBr-S3HjhKbIwn5' #Rails.application.secrets.recaptcha_site_key
-  config.secret_key = '6LdoIl8UAAAAANIYgnwmY-sc9GBpZWCOQC5LZsDg' #Rails.application.secrets.recaptcha_secret_key
+  config.site_key = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY'] 
 end
