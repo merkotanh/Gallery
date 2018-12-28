4635132ca52c8ece7b435751025c4d96c2fc603a
merkotanh <galina.merkotan@faceit-team.com>
2018-12-28 10:32:50 +0200

    settings 4 fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,11 @@
     config.cache_dir = 'carrierwave' # ...two lines  
     
     config.fog_credentials = {
-       use_iam_profile:       false,                        # optional, defaults to false
+       # use_iam_profile:       false,                        # optional, defaults to false
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => 'us-east-2'
+      # :region                => 'us-east-2'
     }
 
     config.fog_directory = ENV['S3_BUCKET']
