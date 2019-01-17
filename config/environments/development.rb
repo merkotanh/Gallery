e255d4826f0790be51b1771622ac1b1a160501ef
merkotanh <galina.merkotan@faceit-team.com>
2019-01-17 18:00:06 +0200

    divise vars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,19 +64,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # config.action_mailer.delivery_method = :smtp
-
-  # config.action_mailer.smtp_settings = {
-  #   address:              "smtp.gmail.com",
-  #   port:                 587,
-  #   domain:               "gmail.com",
-  #   authentication:       "plain",
-  #   enable_starttls_auto: true,
-  #   user_name:            "gala.mrt@gmail.com",
-  #   password:             ""
-  # }
-
-
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,27 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { :host => 'yoursite.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'yoursite.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'https://radiant-shelf-39196.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://radiant-shelf-39196.herokuapp.com'
 
   
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'gala.mrt@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'exhample.com',
+    user_name:            ENV['EMAIL_PASSWORD'],
+    password:             ENV['EMAIL_LOGIN'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
