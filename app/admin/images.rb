cfdd25c4cfcef67f8a66a9c46ee225ef9b0e6ab4
merkotanh <galina.merkotan@faceit-team.com>
2019-03-19 13:33:40 +0200

    add comments to active_admin

--- app/admin/images.rb
+++ app/admin/images.rb
@@ -1,15 +1,5 @@
 ActiveAdmin.register Image do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
- permit_params :image, :image_title, :image_description, :category_id, :cached_votes_up, :cached_votes_score, :cached_votes_total
 
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+ permit_params :image, :image_title, :image_description, :category_id, :cached_votes_up, :cached_votes_score, :cached_votes_total
 
 end
