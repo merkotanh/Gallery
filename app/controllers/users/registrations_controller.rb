7f86a8c8a42a2ee9acc3cb0df5d2aecda01695bb
merkotanh <galina.merkotan@faceit-team.com>
2019-03-22 11:28:12 +0200

    sending email fixed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       begin
         MyMailer.welcome_email(current_user).deliver_now
       rescue
-      	render(
-          html: "<script>alert('Could not send email!')</script>".html_safe,
-          layout: 'application'
-        )
+        render html: "<script>alert('Could not send email!')</script>".html_safe
       end
     else
       redirect_to new_user_registration_path
